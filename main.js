94f1c438dff26a6e0bf52bffd208539cea5e59d3
Lea Oertlin <lea.oertlin@student.uibk.ac.at>
2022-05-19 16:02:11 +0200

    Funktoion drawTemperature als Kopie von drawStations (vorläufig)

--- main.js
+++ main.js
@@ -79,16 +79,38 @@ let drawStations = function (geojson) {
 
     }).addTo(overlays.stations);
 }
+//Temperatur
+
+let drawTemperature = function (geojson) {
+
+    L.geoJSON(geojson, {
+        pointToLayer: function (geoJsonPoint, latlng) {
+            //console.log(geoJsonPoint.properties.name);
+            let popup = `
+            <strong>${geoJsonPoint.properties.name}</strong><br> (${geoJsonPoint.geometry.coordinates[2]} m ü. NN)
+        
+             `;
+
+            return L.marker(latlng, {
+                icon: L.icon({
+                    iconUrl: `icons/wifi.png`,
+                    iconAnchor: [16, 37],
+                    popupAnchor: [0, -37]
+                })
+
+            }).bindPopup(popup);
+        }
+
+    }).addTo(overlays.temperature);
+}
+
 // Wetterstationen
 async function loadData(url) {
     let response = await fetch(url);
     let geojson = await response.json();
 
     drawStations(geojson);
-
-
-
-
+    drawTemperature(geojson);
 
 }
 loadData("https://static.avalanche.report/weather_stations/stations.geojson");
