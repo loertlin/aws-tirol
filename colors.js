5f954ab9c4fdd609498e3a27fa5ad0447a073ccd
Lea Oertlin <lea.oertlin@student.uibk.ac.at>
2022-05-19 18:09:13 +0200

    Farben für Wind implementiert

--- colors.js
+++ colors.js
@@ -112,5 +112,42 @@ const COLORS = {
             max: 1000,
             color: "#cd0feb"
         },
+    ],
+    wind: [
+        {
+            min: 0,
+            max: 5,
+            color: "#ffff64"
+        },
+        {
+            min: 5,
+            max: 10,
+            color: "#c8ff64"
+        },
+        {
+            min: 10,
+            max: 20,
+            color: "#c8ff64"
+        },
+        {
+            min: 20,
+            max: 40,
+            color: "#32c8ff"
+        },
+        {
+            min: 40,
+            max: 60,
+            color: "#6496ff"
+        },
+        {
+            min: 60,
+            max: 80,
+            color: "#9664ff"
+        },
+        {
+            min: 80,
+            max: 300,
+            color: "#ff3232"
+        },
     ]
 }
