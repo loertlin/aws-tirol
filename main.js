59867bc05fb09ec0d6fb33bf48014bce7be6bfd4
Lea Oertlin <lea.oertlin@student.uibk.ac.at>
2022-05-19 16:13:05 +0200

    temperatur als L.divIcon anzeigen

--- main.js
+++ main.js
@@ -92,10 +92,9 @@ let drawTemperature = function (geojson) {
              `;
 
             return L.marker(latlng, {
-                icon: L.icon({
-                    iconUrl: `icons/wifi.png`,
-                    iconAnchor: [16, 37],
-                    popupAnchor: [0, -37]
+                icon: L.divIcon({
+                    className: "aws-div-icon",
+                    html: `<span>${geoJsonPoint.properties.LT}</span>`
                 })
 
             }).bindPopup(popup);
